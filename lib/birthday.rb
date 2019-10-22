9522fd60dc71394c31b8ebb497ea944b9bf0a985
Thomas Wright <twwright@gmail.com>
2019-10-22 19:16:16 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,7 @@ def age_appropriate_birthday(birthday_kids)
     if birthday_kids[name] < 12
       puts "Happy Birthday #{name}! You are now #{age} years old!"
     else
-      puts "Womp womp, sorry. You're too old for birthdays, loser!"
+      puts "You are too old for this."
     end
   }
 end
