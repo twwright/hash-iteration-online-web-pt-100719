7d2c6766c8542412f79a8ce9047cc76e80ab90b8
Thomas Wright <twwright@gmail.com>
2019-10-22 19:12:58 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,7 +7,7 @@
 def happy_birthday(birthday_kids)
   birthday_kids.each {|name, age| 
     if birthday_kids[age] < 12
-      puts "Happy Birthday #{name}! You are now #{age} years old!"}
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
     else
       puts "Womp womp, sorry. You're too old for birthdays, loser!"
     end
