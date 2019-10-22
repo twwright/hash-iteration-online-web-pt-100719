8c64b831a9b0b9963f4bc71beffac9113ca3336b
Thomas Wright <twwright@gmail.com>
2019-10-22 19:16:01 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,6 +5,10 @@
  }
 
 def happy_birthday(birthday_kids)
+  birthday_kids.each {|name, age| puts "Happy Birthday #{name}! You are now #{age} years old!"  }
+end
+
+def age_appropriate_birthday(birthday_kids)
   birthday_kids.each {|name, age| 
     if birthday_kids[name] < 12
       puts "Happy Birthday #{name}! You are now #{age} years old!"
